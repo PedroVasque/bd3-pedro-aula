edfffc2892b0508911dde26736bc248849298717
PedroVasque <pr529017@gmail.com>
2024-08-06 20:42:07 -0300

    Criação do BD

---
+++ create-database.mongodb.js
@@ -0,0 +1,11 @@
+/* NOME DO BANCO DE DADOS */
+const database = 'BD3-PEDRO-AULA'
+
+/* NOME DO BANCO DE DADOS */
+const collection = 'LIVRARIA'
+
+/* CRIAR OU ACESSA O BANCO DE DADOS: */
+use(database)
+
+/* CRIAR UMA COLEÇÃO DE DADOS: */
+db.createCollection(collection)
